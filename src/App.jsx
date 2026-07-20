945dd4545cc1323ab44166e753afbd0552c747c0
Nadhief Athallah Isya <nadhiefathallahi@gmail.com>
2026-07-20 17:21:59 +0700

    oke

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,23 @@
-// src/App.jsx
+import { useState } from "react";
 
-import { Link, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ProductPage from "./pages/ProductPage";
 
-import { PublicLayout } from "./core/layouts";
-import { publicRoutes } from "./pages/Routes";
-
-import "./pages/LandingPage/LandingPage.css";
+import "./App.css";
 
 function App() {
-  const landingPage = (
-    <section className="landing-page">
-      <div className="container">
-        <div className="landing-page__content">
-          <h1 className="landing-page__title">Paradise Nursery</h1>
-
-          <p className="landing-page__description">
-            Bring nature closer to your everyday life with our carefully
-            selected collection of beautiful, healthy houseplants. Whether
-            you're a beginner or an experienced plant lover, Paradise Nursery
-            has the perfect plants to brighten your home.
-          </p>
-
-          <Link to="/products" className="landing-page__button">
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </section>
-  );
+  const [showProductList, setShowProductList] = useState(false);
 
   return (
-    <Routes>
-      <Route path="/" element={landingPage} />
-
-      <Route element={<PublicLayout />}>
-        {publicRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Route>
-    </Routes>
+    <div className="app-container">
+      {!showProductList ? (
+        <LandingPage
+          onGetStarted={() => setShowProductList(true)}
+        />
+      ) : (
+        <ProductPage />
+      )}
+    </div>
   );
 }
 
